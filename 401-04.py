1e087a3dcceacd1f7e8ec2432893098b96f5be75
koangel961103 <61896381+koangel961103@users.noreply.github.com>
2020-04-17 22:05:59 +0800

    Add files via upload

--- 401-04.py
+++ 401-04.py
@@ -7,7 +7,16 @@ Created on Sat Mar 28 10:04:47 2020
 from mcpi.minecraft import Minecraft
 mc=Minecraft.create()
 x,y,z=mc.player.getTilePos()
-mc.setBlocks(x+5 ,y+5 ,z+5 ,x-5 ,y-5 ,z-5 ,5 ,2)
-mc.setBlocks(x+4 ,y+4 ,z+4 ,x-4 ,y-4 ,z-4 ,0)
-mc.setBlocks(x+4 ,y+5 ,z+4 ,x-4 ,y+5 ,z-4 ,0)
-mc.setBlock(x ,y ,z, 169)
+try:
+    blockType=int(input("請輸入要放的方塊ID:"))
+    h=int(input("請輸入屋子的高度:"))
+    w=int(input("請輸入屋子的寬度:"))
+    l=int(input("請輸入屋子的長度:"))
+    hblockType=int(input("請輸入要的屋頂方塊ID:"))
+    mc.setBlocks(x+l ,y+h ,z+w ,x-l ,y ,z-w ,blockType)
+    mc.setBlocks(x+l-1 ,y+h-1 ,z+w-1 ,x-l+1 ,y+1,z-w+1,0)
+    mc.setBlocks(x+l-1 ,y+h-1 ,z+w-1 ,x-l+1 ,y+h,z-w+1,hblockType)
+    
+    mc.postToChat("blockType"+str(blockType))
+except:
+    print("只能輸入數字!!!")
